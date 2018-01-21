96d612bb47261b4667c3247e28ba4c8888c4cf6c
cashewnut <xiyaoguo@yeah.net>
2018-01-21 19:35:30 +0800

    xiyaoguo

--- src/main/java/com/fudanse/graphmatch/ProjectToGraph.java
+++ src/main/java/com/fudanse/graphmatch/ProjectToGraph.java
@@ -668,7 +668,7 @@ public class ProjectToGraph {
 		 * map = new HashMap<>(); for (MethodDeclaration method : methods) {
 		 * pg.createMethod(method, map); } body.size();
 		 */
-		String basePath = "/Users/xiyaoguo/Documents/workspace/Android Studio/";
+		String basePath = "/home/fdse/xiyaoguo/javacode/slidescreen/";
 		File file = new File(basePath);
 		ProjectToGraph pg = null;
 		if (file.isDirectory()) {
